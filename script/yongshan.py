19cb1d6f9570f4d8b3b41abf60f082c6abd12685
Tan, Yoke Liang <yoke.liang.tan@intel.com>
2025-07-06 23:56:35 -0700

    [GenAI] Automatic Derive FPGA hardware config constraints. https://jira-enclave.altera.com/browse/PSGDMX-4501

--- script/yongshan.py
+++ script/yongshan.py
@@ -57,25 +57,65 @@ def main(args):
     pkg_content = open(args.pkgfile, 'r').read()
 
     ### PROMPT for llm
+
     prompt = f"""
 You are an expert in SystemVerilog.  
+Let's think step by step to solve the problem.  
+
 You are given a SystemVerilog package file and a parameter file.   
-<YongShan, please fill in the rest of your prompt here .....>
+
+**INSTRUCTION**
+1. Cross-reference the listed down the extracted parameter to map with the correct Enum list based on the package name and package type. 
+2. Report out the mapped information in yaml format, whereby:
+   a. The top level key is "bcm_attributes".
+   b. follow by the parameter name as each key of dictionary.
+   c. under each parameter name key, 
+      - attr_desc which is similar to the parameter name, 
+      - configuration_type which is set to "CUSTOM", 
+      - function_type which is set to "VIRTUAL", 
+      - default is the default value of the parameter, 
+      - sv_type which is combination of "package_name::package_type", 
+      - attr_use_type is set to "none", 
+      - attr_constrains_pwr_model is set to "false", 
+      - affects_timing is set to "false", 
+      - affects_power is set to "false"
+      - settings is the list of enum value.
+
+Do not include any other text or explanation in the response.    
+Only provide the generated final response in yaml string (without any markdown or backtick)!    
+
 
 **Parameter File:**  
 {param_content}  
-
+  
+  
 **Package File:**
 {pkg_content}
+"""
 
-
+    prompt += """
+`idndef PSG_STD_PKG
+`define PSG_STD_PKG
+package psg_std_pkg;
+typedef enum bit[0:0] {FALSE=0, TRUE=1} true_false_type;
+typedef enum bit[0:0] {DISABLE=0, ENABLE=1} enable_disable_type;
+typedef enum bit[0:0] {OFF=0, ON=1} on_off_type;
+typedef enum bit[0:0] {POWERDOWN_MODE_TRUE=0, POWERDOWN_MODE_FALSE=1} powerdown_mode_false_true_type;
+endpackage // psg_std_pkg
+`endif // PSG_STD_PKG
     """
 
     ba.kwargs['messages'] = [{'role': 'user', 'content': prompt}]
     res = ba.run()
     print(res.message.content)
 
-    return True   
+    ### Write the response to a file
+    with open(args.outfile, 'w') as outfile:
+        # Remove newline characters and markdown formatting
+        cleaned_response = remove_markdown(res.message.content)
+        outfile.write(cleaned_response)
+    print("Output written to:", args.outfile)
+
 
 
 
@@ -98,7 +138,7 @@ def remove_markdown(json_str):
     """Remove markdown formatting from a JSON string."""
     import re
     # Remove code block start and end
-    json_str = re.sub(r"```json", '', json_str)
+    json_str = re.sub(r"```yaml", '', json_str)
     json_str = re.sub(r"```", '', json_str)
     # Remove inline code
     json_str = re.sub(r"`", '', json_str)
@@ -115,6 +155,7 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true', default=False, help='Debug mode')
     parser.add_argument('--paramfile', default=None, required=True, help='param file')
     parser.add_argument('--pkgfile', default=None, required=True, help='pkg file')
+    parser.add_argument('--outfile', default=None, required=True, help='output file')
     args = parser.parse_args()
 
     main(args)
