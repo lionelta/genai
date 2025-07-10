b8c3e8ef22b94a22556d292ee6b83132174e280c
Tan, Yoke Liang <yoke.liang.tan@intel.com>
2025-07-10 00:20:16 -0700

    periodic

--- script/test_with_image.py
+++ script/test_with_image.py
@@ -6,13 +6,25 @@ import sys
 import base64
 sys.path.insert(0, '/nfs/site/disks/da_scratch_1/users/yltan/depot/da/infra/genai/main')
 from lib.agents.base_agent import BaseAgent
+import lib.genai_utils as utils
 
 def main():
-    base64_image = get_base64_image('./aidash.png')
-    prompt = 'summarize what u see in this image, and tabulate out the design scores.'
+    if len(sys.argv) < 3 or '--help' in sys.argv or '-h' in sys.argv:
+        print("""Usage: python test_with_image.py <image_path> <prompt>""")
+        return True
+
+    level = logging.INFO
+    if '--debug' in sys.argv:
+        level = logging.DEBUG
+    logging.basicConfig(format='[%(asctime)s] - %(levelname)s-[%(module)s]: %(message)s', level=level)
+
+    image_path = sys.argv[1]
+    prompt = sys.argv[2]
+    base64_image = get_base64_image(image_path)
 
     ### Openai format
     os.environ['AZURE_OPENAI_API_KEY'] = 'show me the money'
+    os.environ['AZURE_OPENAI_MODEL'] = 'gpt-4o'
     a = BaseAgent()
     a.kwargs['messages'] = [
         {"role": "user", "content": [
@@ -28,7 +40,8 @@ def main():
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    
+   
+    '''
     ### Ollama format
     del os.environ['AZURE_OPENAI_API_KEY']
     a = BaseAgent()
@@ -44,12 +57,12 @@ def main():
     print(res)
     print("======================")
     print(res.message.content)
+    '''
 
 def get_base64_image(image_path):
     with open(image_path, 'rb') as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 if __name__ == '__main__':
-    logging.basicConfig(format='[%(asctime)s] - %(levelname)s-[%(module)s]: %(message)s', level=logging.INFO)
     main()
 
